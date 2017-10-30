af6fa32c7a3cdebf2f3a8b7c9c789f52426b2a19
marko <maartlis@inbox.lv>
2017-10-31 00:55:22 +0200

    Viss darbojas, izņemot extendPassingTerm izvēli. šis ir gala variants lesson0

--- src/main/java/CreditApplication.java
+++ src/main/java/CreditApplication.java
@@ -17,35 +17,61 @@ public class CreditApplication {
 
 		List<Customer> loanList = new ArrayList<>();
 
-		boolean loginFlag = false;
-		Customer cs = new Customer();
+		Customer logInCustomer = new Customer();
+		boolean logInFlag = false;
 
 		printProgramMenu();
+		int menuItem = 1;
 		while(true){
-			int menuItem = getFromMenuItemToExecute();
-			if(menuItem == 6){
+			if(menuItem == 7){
+				System.out.println("System is shutting down ...");
 				break;
 			}
-			cs = logInSystem(loanList);
+			menuItem = getFromMenuItemToExecute();
+
 			switch(menuItem){
-				case 1: {
-					cs = logInSystem(loanList);
-					break;
-				}
-				case 2:{
-					takeLoan(loanList, cs);
-					break;
-				}
-				case 3:{
-					extendLoanPassingTerm();
-					break;
-				}
-				case 4:{
-					viewUserLoans();
+				case 1:{
+					logInCustomer = logInSystem(loanList);
+					System.out.println(logInCustomer.getName() +", you are logged in system...");
+					logInFlag = true;
 					break;
 				}
 				case 5:{
 					printProgramMenu();
+					break;
+				}
+				default:{
+					System.out.println("Wrong choose. \nPlease press 1 to log in system");
+				}
+			}
+
+			while(logInFlag == true){
+				menuItem = getFromMenuItemToExecute();
+				if(menuItem == 6 || menuItem == 7){
+					logInFlag = false;
+					System.out.println(logInCustomer.getName() + " you are logging out... ");
+					break;
+				}
+
+				switch(menuItem){
+					case 2:{
+						takeLoan(logInCustomer);
+						break;
+					}
+					case 3:{
+						extendLoanPassingTerm(logInCustomer);
+						break;
+					}
+					case 4:{
+						viewUserLoans(logInCustomer);
+						break;
+					}
+					case 5:{
+						printLoginProgramMenu();
+					}
+					default:{
+						System.out.println("Wrong choose, please press 5 to see program menu");
+					}
 				}
 			}
 		}
@@ -54,7 +80,6 @@ public class CreditApplication {
 	private static Customer logInSystem(List<Customer> loanList){
 		System.out.println("Enter Name");
 		Scanner sc = new Scanner(System.in);
-
 		String LoginName = sc.nextLine();
 		Customer existingCustomer = searchForCustomer(LoginName, loanList);
 		if(existingCustomer != null){
@@ -63,11 +88,12 @@ public class CreditApplication {
 			System.out.println("You are new customer");
 			Customer newCustomer = new Customer();
 			newCustomer.setName(LoginName);
+			loanList.add(newCustomer);
 			return newCustomer;
 		}
 	}
 
-	private static void takeLoan(List<Customer> loanList, Customer customer){
+	private static void takeLoan(Customer customer){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter loan amount");
 		double amount = Double.parseDouble(sc.nextLine());
@@ -77,14 +103,25 @@ public class CreditApplication {
 		customer.addPassingTerm(term);
 	}
 
-	private static void viewUserLoans(List<Customer> loanList){
-
+	private static void viewAllUsersLoans(List<Customer> loanList){
+		for(Customer customer : loanList ){
+			System.out.println(customer.getName() + "-->"+ customer.getLoanAmount() +"->" + customer.getPassingTerm());
+		}
 	}
 
-	private static void extendLoanPassingTerm(){
+	private static void viewUserLoans(Customer loginCustomer){
+		System.out.println(loginCustomer.getName() + " loans are:\n" +
+							"Loans Amount, EUR  --> " + loginCustomer.getLoanAmount() +"\n"+
+							"Loans passing term --> " + loginCustomer.getPassingTerm());
 	}
 
-
+	private static void extendLoanPassingTerm(Customer loginCustomer){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter new passing term: ");
+		double extendedPassingTerm = Double.parseDouble(sc.nextLine());
+		loginCustomer.getPassingTerm().add(extendedPassingTerm);
+		loginCustomer.getLoanAmount().get(loginCustomer.getLoanAmount().size());
+	}
 
 	private static int getFromMenuItemToExecute(){
 		System.out.println("Enter menu item number:");
@@ -94,12 +131,17 @@ public class CreditApplication {
 
 	private static void printProgramMenu(){
 		System.out.println("Program Menu:");
-		System.out.println("1. Enter personal info");
+		System.out.println("1. Log in system");
+		System.out.println("5. Print program menu");
+		System.out.println("7. Exit from application");
+	}
+	private static void printLoginProgramMenu(){
+		System.out.println("Program Menu:");
 		System.out.println("2. Take a loan");
 		System.out.println("3. Extend passing term");
 		System.out.println("4. View all user loans");
 		System.out.println("5. Print program menu");
-		System.out.println("6. Exit");
+		System.out.println("6. Exit from user");
 	}
 
 	private Customer findCustomer(String customerName, List<Customer> loanList){
